7a408fc1ec7a7da51d7ec344087b01cd57610f0b
RawandDev <rawand.200091384037@gmail.com>
2021-07-18 15:25:46 +0300

    added nav bar

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import Row from "./component/Row";
 import requests from './helper/requests';
 import Banner from "./component/Banner"
+import Nav from "./component/Nav"
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Banner />
       <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} isLarge/>
       <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
