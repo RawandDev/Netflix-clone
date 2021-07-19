b404a41bcb5028b1502517bba3e7c8668aa89db4
RawandDev <rawand.200091384037@gmail.com>
2021-07-19 17:37:44 +0300

    add all categories

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -11,6 +11,11 @@ function App() {
       <Banner />
       <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} isLarge/>
       <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}/>
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
     </div>
   );
 }
