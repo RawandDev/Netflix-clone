7c35b34a5741570387d72754d9699927398daf4b
RawandDev <rawand.200091384037@gmail.com>
2021-07-19 14:54:32 +0300

    added trailer

--- netflix/src/component/Row.js
+++ netflix/src/component/Row.js
@@ -1,10 +1,14 @@
 import { useState, useEffect } from "react";
 import axios from "../helper/axios";
-import "./Row.css"
+import "./Row.css";
+import Youtube from "react-youtube";
+// import movieTrailer from "movie-trailer";
+import ReactPlayer from "react-player";
 
 function Row({ title, fetchUrl, isLarge }) {
   const [movie, setMovie] = useState([]);
   const posterBaseUrl = "https://image.tmdb.org/t/p/original";
+  const [showVideo, setShowVideo] = useState(false)
 
   useEffect(() => {
     async function fetchData() {
@@ -14,23 +18,35 @@ function Row({ title, fetchUrl, isLarge }) {
 
     fetchData();
   }, [fetchUrl]);
-
-  // console.log(movie);
+  
+  
+  const handleClick = () => {
+    console.log("clicked")
+    setShowVideo(!showVideo)
+    console.log(showVideo)
+  }
 
   return (
     <div className="row">
       <h1>{title}</h1>
       <div className="poster_path">
-        {movie.map((mo) => {
+        {movie.map((movie) => {
           return (
             <img
-              key={mo.id}
+              key={movie.id}
+              onClick={handleClick}
               className={`row_poster ${isLarge && "row_posterLarge"}`}
-              src={`${posterBaseUrl}${isLarge ? mo.poster_path : mo.backdrop_path}`}
-              alt={mo.name}
+              src={`${posterBaseUrl}${
+                isLarge ? movie.poster_path : movie.backdrop_path
+              }`}
+              alt={movie.name}
             />
           );
         })}
+      </div>
+      <div className={showVideo ? "true" : "false"}>
+        {/* The class below is when the video shows it will start playing */}
+      <ReactPlayer url="https://www.youtube.com/watch?v=dQw4w9WgXcQ" width="100%" playing={showVideo ? true : false} />
       </div>
     </div>
   );
